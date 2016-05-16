02baf93677cb786481fa0bcb38d26f29054db34f
lsl <ux_lsl@163.com>
2016-05-16 22:34:49 +0800

    version 0.01

---
+++ main.py
@@ -0,0 +1,82 @@
+# -*- coding:utf-8 -*-
+
+import re
+import os
+import sys
+import argparse
+from urllib import request, parse, error
+import requests
+from bs4 import BeautifulSoup
+from ffmpeg import (
+        ffmpeg_concat_ts_to_mkv,
+        ffmpeg_concat_mp4_to_mp4,
+        ffmpeg_concat_av,
+        ffmpeg_concat_flv_to_mp4,
+)
+from utils import url_size, url_save
+
+
+headers = {'Host': 'www.flvcd.com'}
+formats = ['super', 'high', 'low', 'normal']
+code_reg = re.compile(r'charset=([A-Za-z]+)')
+title_reg = re.compile(r'<meta name="title" context="([^"]+)|<title>([^<]+)</title>')
+fmt_reg = re.compile('([^\.]+)$')
+ffmpeg_method = {
+        'ts': (ffmpeg_concat_ts_to_mkv, 'mkv'),
+        'mp4': (ffmpeg_concat_mp4_to_mp4, 'mp4'),
+        'av': (ffmpeg_concat_av, 'av'),
+        'flv':(ffmpeg_concat_flv_to_mp4, 'mp4')
+        }
+
+
+def get_download_urls(url, fmt):
+    params = {'format': fmt, 'kw': url}
+    r = requests.get('http://www.flvcd.com/parse.php', params=params, headers=headers)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    try:
+        table = soup.find_all('td', attrs={'class': 'mn STYLE4', 'align': 'left'})[1]
+        parts = []
+        for i in table.find_all('a'):
+            parts.append(i['href'])
+        return parts
+    except IndexError:
+        return []
+
+
+def download_video(url, format='super'):
+    r = requests.get(url)
+    text = r.text
+    code = code_reg.search(r.text).groups(0)[0]
+    if code:
+        text = r.content.decode(code)
+    title_1, title_2 = title_reg.search(text).groups()
+    title = title_1 or title_2
+    for f in formats:
+        parts = get_download_urls(url, f)
+        if len(parts) > 0:
+            break
+    try:
+        fmt = fmt_reg.search(parts[0]).groups(0)[0]
+        files = []
+        for i, url in enumerate(parts):
+            filename = './{}_{}.{}'.format(title, i, fmt)
+            url_save(url,  filename)
+            files.append(filename)
+        ffmpeg_concat, to_fmt = ffmpeg_method[fmt]
+        output = '{}.{}'.format(title, to_fmt)
+        ffmpeg_concat(files, output)
+        for f in files:
+            os.remove(f)
+    except AttributeError as e:
+        print(e)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='use flvcd to download video')
+    parser.add_argument('--format', default='super', metavar='N', help='default is super, hige, low, normal')
+    parser.add_argument('url', help='download url')
+    args = parser.parse_args()
+    download_video(args.url, args.format)
+
+if __name__ == '__main__':
+    main()
---
+++ utils.py
@@ -0,0 +1,93 @@
+# -*- coding:utf-8 -*-
+
+import os
+from urllib import request, parse, error
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+
+
+def url_size(url, faker = False, headers = {}):
+    if faker:
+        response = request.urlopen(request.Request(url, headers = fake_headers), None)
+    elif headers:
+        response = request.urlopen(request.Request(url, headers = headers), None)
+    else:
+        response = request.urlopen(url)
+
+    size = response.headers['content-length']
+    return int(size) if size!=None else float('inf')
+
+
+def url_save(url, filepath, force=True, refer = None, is_part = False, faker = False, headers = {}):
+    file_size = url_size(url, faker = faker, headers = headers)
+    pbar = tqdm(total=file_size, unit='B', unit_scale=True)
+    if os.path.exists(filepath):
+        if not force and file_size == os.path.getsize(filepath):
+            if not is_part:
+                pbar.close()
+                print('Skipping %s: file already exists' % tr(os.path.basename(filepath)))
+            else:
+                pbar.update(file_size)
+            return
+        else:
+            if not is_part:
+                pbar.close()
+                print('Overwriting %s' % tr(os.path.basename(filepath)), '...')
+    elif not os.path.exists(os.path.dirname(filepath)):
+        os.mkdir(os.path.dirname(filepath))
+
+    temp_filepath = filepath + '.download' if file_size!=float('inf') else filepath
+    received = 0
+    if not force:
+        open_mode = 'ab'
+
+        if os.path.exists(temp_filepath):
+            received += os.path.getsize(temp_filepath)
+            pbar.update_received(os.path.getsize(temp_filepath))
+    else:
+        open_mode = 'wb'
+
+    if received < file_size:
+        if faker:
+            headers = fake_headers
+        elif headers:
+            headers = headers
+        else:
+            headers = {}
+        if received:
+            headers['Range'] = 'bytes=' + str(received) + '-'
+        if refer:
+            headers['Referer'] = refer
+
+        response = request.urlopen(request.Request(url, headers = headers), None)
+        try:
+            range_start = int(response.headers['content-range'][6:].split('/')[0].split('-')[0])
+            end_length = end = int(response.headers['content-range'][6:].split('/')[1])
+            range_length = end_length - range_start
+        except:
+            content_length = response.headers['content-length']
+            range_length = int(content_length) if content_length!=None else float('inf')
+
+        if file_size != received + range_length:
+            received = 0
+            pbar.close = 0
+            open_mode = 'wb'
+
+        with open(temp_filepath, open_mode) as output:
+            while True:
+                buffer = response.read(1024 * 256)
+                if not buffer:
+                    if received == file_size: # Download finished
+                        break
+                    else: # Unexpected termination. Retry request
+                        headers['Range'] = 'bytes=' + str(received) + '-'
+                        response = request.urlopen(request.Request(url, headers = headers), None)
+                output.write(buffer)
+                received += len(buffer)
+                pbar.update(len(buffer))
+
+    assert received == os.path.getsize(temp_filepath), '%s == %s == %s' % (received, os.path.getsize(temp_filepath), temp_filepath)
+
+    if os.access(filepath, os.W_OK):
+        os.remove(filepath) # on Windows rename could fail if destination filepath exists
+    os.rename(temp_filepath, filepath)
